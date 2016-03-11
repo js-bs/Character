eeb0845f388687a3d2d7e8cf9202d554b1401bdd
js-bs <davluzius@outlook.com>
2016-03-11 14:24:12 -0800

    Add character component.

---
+++ Character.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class Character extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    let letter = this.props.letter;
+    if(this.props.isCapital){
+      letter = letter.toUpperCase();
+    }
+    return (
+      <span>
+        {letter}
+      </span>
+    );
+  }
+
+}
+
+Character.propTypes = {
+  letter: React.PropTypes.string,
+  isCapital: React.PropTypes.bool,
+};
+
+export default Character;
